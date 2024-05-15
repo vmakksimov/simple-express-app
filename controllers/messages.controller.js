87ba30600baa100f36f38ae6794af737771b843c
vmaksimov <viktor.maksimov@oblak.bg>
2024-05-15 13:31:56 +0300

    adding simple express app SSR

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,12 @@
+
+const path = require('path');
+
 function getMessages(req, res){
-    res.send('<ul>Getting messages:</ul>')
+    // res.send('<ul>Getting messages:</ul>')
+    res.render('messages', {
+        dogs: ["Nala", "King", "Aya", "Lucky"]
+    })
+    // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'rottie.jpg'));
 }
 function postMessages(req, res){
     console.log('Updating messages... ')
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 const friendsRouter = require('./routes/friends.router')
 const messagesRouter = require('./routes/messages.router')
 
 const app = express();
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
 const PORT = 3000;
 
 app.use((req, res, next) => {
@@ -10,9 +13,17 @@ app.use((req, res, next) => {
     next();
     const delta = Date.now() - start
     console.log(`${req.method} ${req.baseUrl }${req.url} ${delta}ms`)
-})
+});
 
+
+app.use('/site', express.static(path.join(__dirname, 'public')))
 app.use(express.json())
+app.get('/', (req, res) => {
+    res.render('index.hbs', {
+        title: 'Nala the Rottie',
+        caption: 'What a beatiful Rottie!!'
+    })
+})
 app.use('/friends', friendsRouter)
 app.use('/messages', messagesRouter)
 app.listen(PORT, () => {
